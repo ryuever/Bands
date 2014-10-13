e081c30e0330be640ddc4b4212ee8f74067c3428
ryuever <liuyouchao111@gmail.com>
2014-10-14 01:30:58 +0900

    display row cell

---
+++ Bands/WBABandsListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  WBABandsListTableViewController.h
+//  Bands
+//
+//  Created by ryu on 2014/10/13.
+//  Copyright (c) 2014年 ryu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WBABandsListTableViewController : UITableViewController
+
+@end
